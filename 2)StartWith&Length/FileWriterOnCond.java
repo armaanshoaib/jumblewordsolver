934e74ec4a5fa5d8749ca4b971308d7fd2919e82
armaanshoaib <76841030+armaanshoaib@users.noreply.github.com>
2023-05-14 22:26:57 +0530

    Writes only on given condition
    Writes the content from one file (sample.txt) to another file(sample1.txt) based on a given condition such as starting with "j" and has 3 chars

---
+++ 2)StartWith&Length/FileWriterOnCond.java
@@ -0,0 +1,36 @@
+package jumble;
+
+import java.io.*;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FileIO {
+    public static void main(String[] args) {
+
+        try {
+            File fobj = new File("sample.txt");//1)creates a file pointer to file
+            Scanner fileReader = new Scanner(fobj);//3)creates a scanner object for the file pointer
+            File fobj1 = new File("sample1.txt");
+            while (fileReader.hasNextLine()) {// 4) checks whether file has next line
+                String fileData = fileReader.nextLine();//5) creates a string and stores the file data into it
+                fileData = fileData.toLowerCase(Locale.ROOT);
+                try {
+                    FileWriter fr = new FileWriter("sample1.txt",true);
+                    if(fileData.startsWith("j")&&fileData.length()==3) {
+                        fr.write(fileData);
+                        fr.write("\n");
+                    }
+                    fr.close();
+                } catch (IOException e) {
+                    System.out.println("An error occurred.");
+                    e.printStackTrace();
+                }
+                System.out.println(fileData);//6)prints the string fD onto screen
+            }
+        }catch (FileNotFoundException e) {// exception handling while reading from file
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+}
+
