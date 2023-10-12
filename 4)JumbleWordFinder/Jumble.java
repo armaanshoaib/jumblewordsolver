e79bdb559cfedeff9f5e3ffe2904b1031b1769fc
armaanshoaib <76841030+armaanshoaib@users.noreply.github.com>
2023-10-12 20:44:22 +0530

    Update Jumble.java
    - Fixed converting words in the file to lower case

--- 4)JumbleWordFinder/Jumble.java
+++ 4)JumbleWordFinder/Jumble.java
@@ -1,19 +1,19 @@
-package jumble;
+package Jumble;
 
 import java.io.FileNotFoundException;
 //import java.io.FileWriter;
 //import java.io.IOException;
-import java.util.Locale;
+import java.util.Arrays;
 import java.util.Objects;
 import java.util.Scanner;
 import java.io.File;
 
-public class Jumble {
+public class JumbleWordSolver {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the jumbled word : ");
         String jumbleWord = sc.next();//takes input for jumble word
-        jumbleWord=jumbleWord.toLowerCase(Locale.ROOT);
+        jumbleWord=jumbleWord.toLowerCase();
         char [] jumCharArray = jumbleWord.toCharArray();//converts string to char array
         int []jumWordArray = new int[jumCharArray.length];//creates int array of length jumbleWord string
 
@@ -23,19 +23,19 @@ public class Jumble {
             System.out.println(jumCharArray[i] + " " + jumWordArray[i]);
         }
         //BUBBLE SORTING
-        int n = jumWordArray.length;
-        int temp;
-        for(int i=0; i < n; i++) {
-            for (int j = 1; j < (n - i); j++) {
-                if (jumWordArray[j - 1] > jumWordArray[j]) {
-                    //swap elements
-                    temp = jumWordArray[j - 1];
-                    jumWordArray[j - 1] = jumWordArray[j];
-                    jumWordArray[j] = temp;
-                }
-            }
-        }
-
+//        int n = jumWordArray.length;
+//        int temp;
+//        for(int i=0; i < n; i++) {
+//            for (int j = 1; j < (n - i); j++) {
+//                if (jumWordArray[j - 1] > jumWordArray[j]) {
+//                    //swap elements
+//                    temp = jumWordArray[j - 1];
+//                    jumWordArray[j - 1] = jumWordArray[j];
+//                    jumWordArray[j] = temp;
+//                }
+//            }
+//        }
+        Arrays.sort(jumWordArray);
 
         System.out.println("\nSorted Char Array\n");
         for(int i=0;i<jumbleWord.length();i++){
@@ -43,29 +43,34 @@ public class Jumble {
             System.out.println(jumCharArray[i] + " " + jumWordArray[i]);//prints the sorted CHAR array
         }
         System.out.println("------------------------");
+        boolean wordFound = false;
         float start = System.nanoTime();//for timer
         try {// fobj(file-pointer) requires a try and catch block!
             File fobj = new File("D:/Java/Jumble/words.txt");//creates a file pointer to file
             Scanner fileReader = new Scanner(fobj);//creates a scanner object for the file pointer
 
-            //----------------------------------------------------------------------------
 
+            //----------------------------------------------------------------------------
             while (fileReader.hasNextLine()) {// checks whether file has next line
                 String fileData = fileReader.nextLine();// creates a string and stores the file data into it
                 if(Objects.equals(fileData, "")){//EOF
                     break;
                 }
-                char[] posAns = fileData.toCharArray();//converts fileData string to char array
 
-                if (fileData.length() == jumbleWord.length()) {//to reduce time we choose words that have length equal to jumbled word
+                char ch = jumCharArray[0];
+
+                if (fileData.length() == jumbleWord.length()) {
+                    //to reduce time we choose words that have length equal to jumbled word
                     int[] posAnsArray = new int[fileData.length()];//creates an int array of size  string in sample to store ascii vals
-                    for(int x=0; x<posAns.length;x++) {
-                        posAnsArray[x] = posAns[x]-'0';
+                    fileData = fileData.toLowerCase();
+                    char[] posAns = fileData.toCharArray();//converts fileData string to char array
+                    for (int x = 0; x < posAns.length; x++) {
+                        posAnsArray[x] = posAns[x] - '0';
                     }
                     //BUBBLE SORTING fileData char array(posAnsArray)
                     int temp1;
                     int num = posAns.length;
-                    for(int iter=0; iter < num; iter++) {
+                    for (int iter = 0; iter < num; iter++) {
                         for (int j = 1; j < (num - iter); j++) {
                             if (posAnsArray[j - 1] > posAnsArray[j]) {
                                 //swap elements
@@ -76,29 +81,29 @@ public class Jumble {
                         }
                     }
                     //converting back to char from int array after getting sorted by ADDING zero
-                    for(int al=0;al<posAns.length;al++){
-                        posAns[al]=(char)(posAnsArray[al] + '0');
+                    for (int al = 0; al < posAns.length; al++) {
+                        posAns[al] = (char) (posAnsArray[al] + '0');
                     }
-                    int match=0;
+                    int match = 0;
                     //PRINTS THE JUMBLED WORD IF IT IS IN THE WORDS.TXT
-                    for(int charMatch=0;charMatch<posAns.length;charMatch++){
-                        if(charMatch>=jumCharArray.length){
-                            break;
-                        }
-                        if(posAns[charMatch]==jumCharArray[charMatch]){
+                    for (int charMatch = 0; charMatch < posAns.length; charMatch++) {
+                        if (posAns[charMatch] == jumCharArray[charMatch]) {
                             match++;
-
                         }
                     }
-                    if(match==posAns.length){
+                    if (match == posAns.length) {
                         System.out.println(fileData);
+                        wordFound = true;
                     }
-                     // i++;//advances index of jumCharArray
+                    // i++;//advances index of jumCharArray
                 }
+
             }
         }catch (FileNotFoundException e) {// exception handling while reading from sample file
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            System.out.println( e);
+        }
+        if(!wordFound){
+            System.out.println("No Words Found!");
         }
 
         float end = System.nanoTime();
@@ -106,4 +111,3 @@ public class Jumble {
         System.out.println("\nElapsed Time : " + (elapsedTime/1000000000)+ " sec.\n");
     }
 }
-
